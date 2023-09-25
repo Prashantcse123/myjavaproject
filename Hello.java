4c47b9c8c1032df48ae53668f4a767ce91e12ac4
Prashantcse123 <41784908+Prashantcse123@users.noreply.github.com>
2023-09-25 11:16:19 +0530

    Update Hello.java at 11:15 am

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Hello {
 
-public static int addition(int a,int b){
+public static int summation(int a,int b){
       return (a+b);
 }
 
@@ -14,7 +14,7 @@ public static void main(String a[])
     a = sc.nextInt();
     System.out.print("Enter b:");
     b = sc.nextInt();
-    res = Hello.addition(a,b);
+    res = Hello.summation(a,b);
     System.out.print("res = " + res);
 }
 
