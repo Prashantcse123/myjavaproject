d82cb411125dd53212f1ea0e6b60f119c5131f29
Prashantcse123 <41784908+Prashantcse123@users.noreply.github.com>
2023-09-25 11:18:27 +0530

    Update Hello.java at 11:18 am

--- Hello.java
+++ Hello.java
@@ -2,20 +2,15 @@ import java.util.Scanner;
 
 public class Hello {
 
-public static int summation(int a,int b){
-      return (a+b);
-}
-
 public static void main(String a[])
 {
-    int a,b,res;
+    float a,b,res;
     Scanner sc =  new Scanner(System.in);
     System.out.print("Enter a:");
-    a = sc.nextInt();
+    a = sc.nextFloat();
     System.out.print("Enter b:");
-    b = sc.nextInt();
-    res = Hello.summation(a,b);
-    System.out.print("res = " + res);
+    b = sc.nextFloat();
+    System.out.print("res = " + (a+b));
 }
 
 }
